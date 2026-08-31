de84107b8611a8485e0668ee53f5659ee34fd48b
권순원 <wkdrn6177@gmail.com>
2026-08-31 21:45:16 +0900

    Item 구조.v1

---
+++ Assets/Scripts/ExpressionManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class ExpressionManager : MonoBehaviour
+{
+    public List<Item> itemList;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
