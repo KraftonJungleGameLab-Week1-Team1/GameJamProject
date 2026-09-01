1ea6e90073c68e6907e7c55c57aa3247e09fda72
mingyu243 <jmg6017@gmail.com>
2026-09-01 13:24:41 +0900

    ItemFactory 추가

---
+++ Assets/Scripts/ItemFactory.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class ItemFactory : MonoBehaviour
+{
+    public static ItemFactory Instance;
+
+    [Header("순서 지켜줘야 함 1 ~ 9")]
+    public Item[] ItemNumberPrefabs;
+
+    public Item ItemPlusPrefab;
+    public Item ItemMinusPrefab;
+    public Item ItemMultiplyPrefab;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public Item Instantiate(ItemData data)
+    {
+        Item item = null;
+
+        if (data.Type == EItemType.Number)
+        {
+            int index = data.NumberValue - 1;
+            item = Instantiate(ItemNumberPrefabs[index]);
+        }
+        else if (data.Type == EItemType.Operator)
+        {
+            if (data.OperatorType == EItemOperatorType.Plus)
+            {
+                item = Instantiate(ItemPlusPrefab);
+            }
+            else if (data.OperatorType == EItemOperatorType.Minus)
+            {
+                item = Instantiate(ItemMinusPrefab);
+            }
+            else if (data.OperatorType == EItemOperatorType.Multiply)
+            {
+                item = Instantiate(ItemMultiplyPrefab);
+            }
+        }
+
+        return item;
+    }
+}
