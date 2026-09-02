1644d555ebcbcb97148e3987c0b063e0d9e71dd5
Songunyoung-jungle <perjury@dgu.ac.kr>
2026-09-02 10:21:42 +0900

    메인메뉴 수정
    ㅁㅇㄴㄹ

--- Assets/sgy/MainMenuManager.cs
+++ Assets/sgy/MainMenuManager.cs
@@ -6,7 +6,7 @@ public class MainMenuManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void Play()
     {
-        SceneManager.LoadScene("sgy");    
+        SceneManager.LoadScene("Main");    
     }
 
     // Update is called once per frame
